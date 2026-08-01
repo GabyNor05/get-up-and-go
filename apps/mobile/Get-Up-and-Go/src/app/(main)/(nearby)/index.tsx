fe0c54448365d66182eec5d06a6959c8218af5a2
GabyNor05 <241143@virtualwindow.co.za>
2026-08-01 21:49:19 +0200

    mobile project reset

--- apps/mobile/Get-Up-and-Go/src/app/(main)/(nearby)/index.tsx
+++
@@ -1,9 +0,0 @@
-import { View } from 'react-native';
-
-const NearbyScreen = () => {
-  return (
-    <View></View>
-  )
-}
-
-export default NearbyScreen
--- apps/mobile/Get-Up-and-Go/src/app/(main)/(settings)/index.tsx
+++
@@ -1,9 +0,0 @@
-import { View } from 'react-native';
-
-const SettingsScreen = () => {
-  return (
-    <View>index</View>
-  )
-}
-
-export default SettingsScreen
